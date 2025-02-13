239dca180638d9b9482b70690e27909fe5e2392f
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-02-13 16:41:32 +0800

    get the file and read the data

--- app/App.php
+++ app/App.php
@@ -1,3 +1,36 @@
 <?php
 
 declare(strict_types = 1);
+
+function getTransactionFiles(string $dirPath): array  // get all files path
+{
+    $files = [];
+
+    foreach (scandir($dirPath) as $file) { // loop through all files in the directory
+        if (is_dir($file)) { // if the file is directory
+            continue;
+        }
+
+        $files[] = $dirPath . $file;
+    }
+    return $files;
+}
+
+function getTransaction(string $fileName): array { // read csv file and return data
+    if(! file_exists($fileName)) { // check the file exist or not
+        trigger_error("File $fileName does not exist", E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r'); //open file and read
+
+    fgetcsv($file); //read the first line here. To make sure the first won't be saved in $transactions
+
+    $transactions = [];
+
+    //read every line of csv data, each line  of data will save in $transactions[]
+    while (($transaction = fgetcsv($file)) !== false) { 
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
